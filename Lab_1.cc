0395103da477dc3615aab3e2c0ab6ac3dd97111d
Abir Hasan <81650568+Abirsonet563@users.noreply.github.com>
2022-11-16 09:00:58 +0800

    Create Lab_1.cc

---
+++ Lab_1.cc
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+//main function
+int main() {
+    // count is a function that writes to the console output
+    cout<< "Hello World" <<endl;
+    
+    //variable
+    string line;
+    
+    cout <<"Enter A String: "<< endl;
+    cin >>line; //cin function reads from console
+    cout << "\n>> You Have Keyed In " <<line <<endl;
+
+    return 0;
+}
