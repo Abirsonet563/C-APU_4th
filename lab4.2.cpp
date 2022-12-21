8cc775def88b75cbbfb1d022446f5a7dc69b3534
Abir Hasan <81650568+Abirsonet563@users.noreply.github.com>
2022-12-21 14:44:26 +0800

    new1.0

--- lab4.2.cpp
+++ lab4.2.cpp
@@ -13,3 +13,12 @@ struct BankAccount
     double get_rate(); //Return the current interest rate
     void set_rate(double rate); // rate is set to interest rate
 };
+
+// Another code (for the tut4/partb/exe2 - insert another Point structure to connect to the linkedlist (p1->p2->p3))
+...
+Point * p3 = new Point;
+p3->x = 70;
+p3->y = 6;
+p3->nextaddress = NULL;
+p2->nextaddress = p3;
+...
